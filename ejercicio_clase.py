5274631f60a2617302fadfd6a1eaa102496f5c5e
Mateoochoagaleano <158522384+Mateoochoagaleano@users.noreply.github.com>
2024-04-25 17:41:03 -0500

    MATEO.OCHOA_EJERCICIO.EN_CLASE
    MATEO.OCHOA_EJERCICIO.EN_CLASE

---
+++ ejercicio_clase.py
@@ -0,0 +1,13 @@
+list_lote_urbano = []
+list_lote_comercial = []
+list_lote_campestre = []
+
+import os
+
+def fnt_limpiar():
+    os.system('cls')
+    print('AUTOR: Mateo Ochoa \nFECHA: 31 DICIEMBRE 2024\n')
+    
+    
+    
+def fnt_registrar()
